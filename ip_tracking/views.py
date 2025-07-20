023f8f36daad41ebea8514ac5b9ec92d4a475269
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-20 21:13:27 +0300

    Update views.py

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from ratelimit.decorators import ratelimit
 
 # Create your views here.
+class RateLimitedLoginView(LoginView):
+    template_name = 'login.html'
+
+    @ratelimit(key='ip', rate='5/m', method='POST', block=True)
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            # Higher limit for authenticated users (handled by not applying ratelimit here)
+            return super().post(request, *args, **kwargs)
+        return super().post(request, *args, **kwargs)
