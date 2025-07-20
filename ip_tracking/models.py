3b8f837d2586a8ca7343f2a3dd6c861d9e29d454
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-20 20:55:13 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class RequestLog(models.Model):
+    ip_address = models.CharField(max_length=45)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['ip_address']),
+            models.Index(fields=['timestamp']),
+        ]
