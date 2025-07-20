3f0b3403e640b27e86475d1e7a4d14c94fcad57e
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-20 20:47:59 +0300

    Update settings.py

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qlud!w(n#y8w0^)c)+#w1_p@1+ovsl(dzc_olffep$x(&&r-l4'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+# SECRET_KEY = 'django-insecure-qlud!w(n#y8w0^)c)+#w1_p@1+ovsl(dzc_olffep$x(&&r-l4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
