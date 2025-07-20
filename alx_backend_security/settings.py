a0c43c1255e40a3ec958c41def3f7cf12a4ffc31
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-20 20:52:12 +0300

    Update settings.py

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -22,7 +22,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! {python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"}
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECRET_KEY = 'django-insecure-qlud!w(n#y8w0^)c)+#w1_p@1+ovsl(dzc_olffep$x(&&r-l4'
 
