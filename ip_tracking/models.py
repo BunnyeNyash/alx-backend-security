7244e2495a30f31d2c4b54646f1e7f694257bc5c
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-20 21:08:37 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -5,6 +5,8 @@ class RequestLog(models.Model):
     ip_address = models.CharField(max_length=45)
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} at {self.timestamp}"
